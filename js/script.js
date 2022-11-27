a0a7d7b8a08193d74f6d9dcd90d5c42bfda0176f
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2022-11-27 15:23:04 +0200

    chore: actualizacao de rota mais curta

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
 //mapa inicializado
-var map = L.map('map').setView([-19.796382, 34.884487], 7);
+var map = L.map('map').setView([-25.953724, 32.588711], 16);
 
 //camada osm
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 })
 osm.addTo(map);
+
+//icone para o marcador
+var carIcon = L.icon({
+    iconUrl:'../img/gps.png',
+    iconSize: [70, 70]
+})
+
+//marcador no mapa
+var marker = L.marker([-25.953724, 32.588711]).addTo(map)
+
+//evento de click no mapa
+map.on('click', function (e) {
+    var segundoMarcador = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map)
+
+    L.Routing.control({
+        waypoints: [
+            L.latLng(-25.953724, 32.588711),
+            L.latLng(e.latlng.lat, e.latlng.lng)
+        ]
+    }).on('routesfound', function (e) {
+        console.log(e)
+        e.routes[0].coordinates.forEach(function (coord, index) {
+            setTimeout(() => {
+                marker.setLatLng([coord.lat, coord.lng])
+            }, 100 * index);
+        })
+    })
+    
+    .addTo(map);
+
+})
+
