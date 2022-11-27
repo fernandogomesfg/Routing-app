0c818b028d341f6081cdfe8edb40106266134a42
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2022-11-27 16:51:32 +0200

    chore: routing app

--- js/script.js
+++ js/script.js
@@ -9,32 +9,19 @@ osm.addTo(map);
 
 //icone para o marcador
 var carIcon = L.icon({
-    iconUrl:'../img/gps.png',
+    iconUrl: '../img/gps.png',
     iconSize: [70, 70]
 })
 
 //marcador no mapa
-var marker = L.marker([-25.953724, 32.588711]).addTo(map)
+//var marker = L.marker([-25.953724, 32.588711]).addTo(map)
 
-//evento de click no mapa
-map.on('click', function (e) {
-    var segundoMarcador = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map)
-
-    L.Routing.control({
-        waypoints: [
-            L.latLng(-25.953724, 32.588711),
-            L.latLng(e.latlng.lat, e.latlng.lng)
-        ]
-    }).on('routesfound', function (e) {
-        console.log(e)
-        e.routes[0].coordinates.forEach(function (coord, index) {
-            setTimeout(() => {
-                marker.setLatLng([coord.lat, coord.lng])
-            }, 100 * index);
-        })
-    })
-    
-    .addTo(map);
-
-})
+L.Routing.control({
+    waypoints: [
+        L.latLng([-25.953724, 32.588711]),
+        L.latLng(-25.923909, 32.465802)
+    ],
+    routeWhileDragging: true,
+    geocoder: L.Control.Geocoder.nominatim()
+}).addTo(map);
 
